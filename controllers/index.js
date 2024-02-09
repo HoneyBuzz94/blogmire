6d7cd3d5af384e84c670cd68f284f56e677e89ff
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-02-09 12:37:22 -0700

    Set up basic routes and views. Connected Bulma to the project

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const home = require('./home');
+const login = require('./login');
+const signup = require('./signup');
 
 router.use('/api', apiRoutes);
+router.use('/', home);
+router.use('/login', login);
+router.use('/signup', signup);
 
 module.exports = router;
